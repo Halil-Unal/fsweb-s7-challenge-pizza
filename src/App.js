62ca487dd7409845b5755381290b23184090d03e
HALİL ÜNAL <125642299+Halil-Unal@users.noreply.github.com>
2023-04-06 22:27:21 +0300

    ilk sayfa yapıldı

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from "react";
-
+import "./App.css";
 const App = () => {
   return (
+    <div className="Top">
     <>
-      <h1>Teknolojik Yemekler</h1>
-      <p>Burdaki kodu silip kendi headerınızı ekleyebilirsiniz</p>
+    
+      <h1>Teknolojik Yemekler  </h1>
+      <h1>Kod Acıktırır </h1>
+      <h1>Pizza, Doyurur</h1>
+      <button> ACIKTIM </button>
+      
     </>
+    </div>
   );
 };
 export default App;
